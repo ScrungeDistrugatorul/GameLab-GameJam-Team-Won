66e5e0d719f6c33538f9f03f9eff347425253a42
Adrian Georgiev <andigebg@gmail.com>
2025-02-05 16:18:17 +0100

    Implementet the crate obstacle asset

--- Joyride Inspired Runner/Assets/Scripts/Spawner.cs
+++ Joyride Inspired Runner/Assets/Scripts/Spawner.cs
@@ -39,11 +39,21 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        GameObject cube = Instantiate(objectsToSpawn[Random.Range(0, objectsToSpawn.Length)], 
-            new Vector3(transform.position.x, (transform.position.y + Random.Range(-4,4)),transform.position.z+5), 
-            Quaternion.identity);
-        
-        cube.transform.localScale = new Vector3(Random.Range(1f,2f), Random.Range(1f,2f), transform.localScale.z);
-        cube.transform.localRotation = Quaternion.Euler(Random.Range(0f,90f), Random.Range(0f,90f), transform.eulerAngles.z);
+        GameObject spawnObj = objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
+
+        if (spawnObj.CompareTag("GroundedEnemy"))
+        {
+            Instantiate(spawnObj, 
+                new Vector3(transform.position.x, (transform.position.y - 3.5f),transform.position.z+5), 
+                Quaternion.identity);
+        }
+        else
+        {
+            GameObject cube = Instantiate(spawnObj, 
+                new Vector3(transform.position.x, (transform.position.y + Random.Range(-4,4)),transform.position.z+5), 
+                Quaternion.identity);
+            cube.transform.localScale = new Vector3(Random.Range(1f,2f), Random.Range(1f,2f), transform.localScale.z);
+            cube.transform.localRotation = Quaternion.Euler(Random.Range(0f,90f), Random.Range(0f,90f), transform.eulerAngles.z);
+        }
     }
 }
